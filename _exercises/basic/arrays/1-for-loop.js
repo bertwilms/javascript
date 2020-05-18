0327b93dba0b67c9828a5ef59b20844393084f03
Wilms Bert <wilms.bert@gmail.com>
2020-05-18 13:35:13 +0200

    Eerste oefening

--- _exercises/basic/arrays/1-for-loop.js
+++ _exercises/basic/arrays/1-for-loop.js
@@ -12,6 +12,9 @@ const list = [1,2,3,4,5,6];
     1. Use a for loop to log every item in the array
 
  */
+for ( let i = 0; i < list.length; i++) {
+    console.log(list[i]);
+}
 
 /*
 
@@ -20,11 +23,23 @@ const list = [1,2,3,4,5,6];
  */
 
 const even = [];
+for ( let i = 0; i < list.length;i++){
+    if ( list[i]%2 === 0 ){
+        even.push(list[i]);
+    }
+}
+console.log ( "de even nummers zijn ", even) ;
 
 /*
 
     3. Use a for loop to square every item in the even list.
 
  */
+console.log ( "====================") ;
 
 const squared = [];
+
+for ( let i=0; i < even.length;i++) {
+    squared.push(Math.pow(even[i], 2));
+}
+console.log(squared);
