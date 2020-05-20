a8edda8369d4b0ca5ed1f6de262dfa5c56163e29
Wilms Bert <wilms.bert@gmail.com>
2020-05-20 17:24:31 +0200

    Finished excercise 2 [ advanced-arrays]

--- _exercises/advanced/arrays/2-reduce.js
+++ _exercises/advanced/arrays/2-reduce.js
@@ -17,46 +17,52 @@ const customers = require('../../../data/customers');
 
     Use the reduce method
  */
-// Showing list, but not with reduced
-console.log  ( " ==========  without Reduce ===============") ;
-const showFormatted = (customer) => customer.name.last + " " + customer.name.first + ": " + customer.balance  ;
-console.log ( customers.map(showFormatted)) ;
+console.log  ( "  1. Display the list of customers as an object that looks like this { name: balance }") ;
 
+const customerObject = customers.reduce( (obj, customer) => {
+    obj[customer.name.first + " " + customer.name.last] = customer.balance ;
+    return obj ;
+},{}) ;
 
-console.log  ( " ==========  with Reduce ===============") ;
- let allCustomers = customers.reduce ( function (AllCustomers, customer) {
-      return  AllCustomers + showFormatted(customer)  ;
- },{});
- console.log ( allCustomers) ;
-
-console.log  (":::::::::::::::::::::::::");
-
-
+console.log ( customerObject) ;
 
 /*
 
     2. Display the full names of all the males that are over 30
 
  */
-console.log  ( " ==========  2. Display the full names of all the males that are over 30") ;
+
+console.log  ( " 2. Display the full names of all the males that are over 30") ;
 const isAbove30 = (item) => item.age > 30 ;
 const isMale = (item) => item.isMale ;
-console.log ( customers.filter(isAbove30 && isMale).map(showFormatted))  ;
+const asFullName = (person) => person.name.last + " " + person.name.first ;
+console.log ( customers.filter(isAbove30 && isMale).map(asFullName))  ;
 
 /*
 
     3. Are all females over 30?
 
  */
+console.log ( "3. Are all females over 30?", customers.every(isAbove30));
 
+const hasblueeyes = ( customer ) => customer.eyeColor === "blue" ;
 /*
 
     4. Are there some females with blue eyes?
-
  */
+console.log ( "4. Are there some females with blue eyes?", customers.some(hasblueeyes)) ;
+
 
 /*
 
     5. Get customers over 25 that are male and display them in one object as { _id: name }
 
  */
+console.log ( "5. Get customers over 25 that are male and display them in one object as { _id: name }");
+const isAbove25 = ( customer) => customer.age > 25 ;
+const customerObject2 = customers.filter(isAbove25 && isMale).reduce((obj, customer) => {
+     obj[customer._id] = customer.name.last + " " + customer.name.first ;
+     return obj ;
+ },{}) ;
+
+console.log ( customerObject2) ;
