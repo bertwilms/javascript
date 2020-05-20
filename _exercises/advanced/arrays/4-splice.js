43a9d0d58060facf76be7b53da51d03e6a59b148
Wilms Bert <wilms.bert@gmail.com>
2020-05-20 21:45:06 +0200

    Finished excercises advanced arrays

--- _exercises/advanced/arrays/4-splice.js
+++ _exercises/advanced/arrays/4-splice.js
@@ -8,21 +8,22 @@
 
 const list = [1,2,3,4];
 
-
 /*
-
     1. Add the number zero as the first element of the array
-
  */
 
+list.unshift (0);
+console.log ( list );
 /*
 
     2. Remove the last element of the array
 
  */
-
+list.splice(-1, 1);
+console.log ( list );
 /*
 
     3. Add [4, 5] to the array
 
  */
+list.push(4,5);
